397dc17152a10db6ca7392d95959e35276dd871a
ahorjia <ali.ghorbani@gmail.com>
2016-05-22 22:33:39 -0700

    First Commit

---
+++ stereoSineProj.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"portaudio"
+	"math"
+	"time"
+	"os"
+	"strconv"
+	"fmt"
+)
+
+const sampleRate = 44100
+
+func main() {
+	arg, err := strconv.ParseFloat(os.Args[1], 32)
+
+	if err != nil {
+		fmt.Printf("Error")
+	}
+
+	portaudio.Initialize()
+	defer portaudio.Terminate()
+	s := newStereoSine(arg, arg, sampleRate)
+	defer s.Close()
+	chk(s.Start())
+	time.Sleep(2 * time.Second)
+	chk(s.Stop())
+}
+
+type stereoSine struct {
+	*portaudio.Stream
+	stepL, phaseL float64
+	stepR, phaseR float64
+}
+
+func newStereoSine(freqL, freqR, sampleRate float64) *stereoSine {
+	s := &stereoSine{nil, freqL / sampleRate, 0, freqR / sampleRate, 0}
+	var err error
+	s.Stream, err = portaudio.OpenDefaultStream(0, 2, sampleRate, 0, s.processAudio)
+	chk(err)
+	return s
+}
+
+func (g *stereoSine) processAudio(out [][]float32) {
+	for i := range out[0] {
+		out[0][i] = float32(math.Sin(2 * math.Pi * g.phaseL))
+		_, g.phaseL = math.Modf(g.phaseL + g.stepL)
+		out[1][i] = float32(math.Sin(2 * math.Pi * g.phaseR))
+		_, g.phaseR = math.Modf(g.phaseR + g.stepR)
+	}
+}
+
+func chk(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
