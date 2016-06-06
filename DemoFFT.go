183b6fdec5b164b4ffa7d93b209c119780f0ecbc
ahorjia <ali.ghorbani@gmail.com>
2016-06-06 04:36:28 -0700

    Max content

--- DemoFFT.go
+++ DemoFFT.go
@@ -9,7 +9,7 @@ import (
 	"os"
 
 	"math/cmplx"
-	"sort"
+	//	"sort"
 
 	"github.com/mjibson/go-dsp/dsputils"
 	"github.com/mjibson/go-dsp/fft"
@@ -45,8 +45,6 @@ func Max(slice []float64) int {
 }
 
 func main() {
-	numSamples := 880
-
 	// Reference: https://play.golang.org/p/2kEKXq-kUV
 	f, err := os.Open("/home/agah/Dropbox/Classes/09 - CSE237B/Lab3/Lab3/output2.wav")
 	check(err)
@@ -55,22 +53,23 @@ func main() {
 	w, err2 := wav.New(f)
 	check(err2)
 
-	numSamples = w.Samples
-	samples, err3 := w.ReadFloats(numSamples)
-	check(err3)
+	numSamples := w.Samples
+	//	numSamples := 34567
+	//	samples, err3 := w.ReadFloats(numSamples)
+	//	check(err3)
 
 	// Equation 3-10.
 	x := func(n int) float64 {
-		val := float64(samples[n])
-		return val
+		//		val := float64(samples[n])
+		//		return val
 
 		//		slice32 := make([]float32, 1000)
 		//		slice64 := convertTo64(slice32)
 
-		//		wave0 := math.Sin(2.0 * math.Pi * float64(n) / 440.0)
-		//		wave1 := 0.5 * math.Sin(2*math.Pi*float64(n)/220.0+3.0*math.Pi/220.0)
+		wave0 := math.Sin(2.0 * math.Pi * float64(n) / 440.0)
+		//		wave1 := 0.5 * math.Sin(2*math.Pi*float64(n) * 220.0+3.0*math.Pi/220.0)
 		//		return wave0 + wave1
-		//		return wave0
+		return wave0
 	}
 
 	// Discretize our function by sampling at 8 points.
@@ -91,7 +90,7 @@ func main() {
 		}
 
 		if r > 0.1 { // !dsputils.Float64Equal(r, 0) {
-			fmt.Printf("X(%d) = %.1f ∠ %.1f°\n", i, r, θ)
+			//			fmt.Printf("X(%d) = %.1f ∠ %.1f°\n", i, r, θ)
 		}
 	}
 
@@ -100,9 +99,10 @@ func main() {
 		mags[i] = cmplx.Abs(X[i])
 	}
 
-	sort.Float64s(mags)
-	//	maxVal := Max(mags)
+	//	sort.Float64s(mags)
+	maxVal := Max(mags)
 	for i := numSamples; i > numSamples-20; i-- {
-		fmt.Printf("X(%d) = %.1f\n", i, mags[i-1])
+		//		fmt.Printf("X(%d) = %.1f\n", i, mags[i-1])
 	}
+	fmt.Printf("X(%d) = %.1f Max Freq = %.2f\n", maxVal, mags[maxVal], float64(numSamples)/float64(maxVal))
 }
